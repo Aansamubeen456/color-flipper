2d0e49b2a294efcefac93cd69ac89f6057107491
aansamubeen <mubeenansa1@gmail.com>
2024-07-20 13:40:05 +0500

    first commit

---
+++ hex.js
@@ -0,0 +1,111 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+
+  if (element) {
+    return element;
+  } else {
+    throw Error(
+      `the element ${selector} you are trying to access does not exist.`
+    );
+  }
+};
+
+const btn = getElement(".container-btn");
+const color = getElement(".color");
+let colrArr = "#";
+
+btn.addEventListener("click", () => {
+  changeClr();
+  colrArr = "#";
+});
+const alphaNums = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const simpleColors = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Grey",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSlateGrey",
+  "LightSteelBlue",
+  "LightYellow",
+];
+console.log(simpleColors.length);
+function changeClr() {
+  for (let i = 0; i < 6; i++) {
+    const randomAlpha = Math.floor(Math.random() * 16);
+    colrArr = colrArr.concat(alphaNums[randomAlpha]);
+  }
+  // console.log(colrArr);
+  const val = (color.textContent = colrArr);
+  // console.log(val);
+  document.body.style.backgroundColor = colrArr;
+}
---
+++ simple.js
@@ -0,0 +1,110 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+
+  if (element) {
+    return element;
+  } else {
+    throw Error(
+      `the element ${selector} you are trying to access does not exist.`
+    );
+  }
+};
+
+const btn = getElement(".container-btn");
+const color = getElement(".color");
+let colrArr = "";
+
+btn.addEventListener("click", () => {
+  changeClr();
+  colrArr = "";
+});
+const simpleColors = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Grey",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSlateGrey",
+  "LightSteelBlue",
+  "LightYellow",
+];
+console.log(simpleColors.length);
+function changeClr() {
+  for (let i = 0; i < simpleColors.length; i++) {
+    const randomAlpha = Math.floor(Math.random() * simpleColors.length);
+    colrArr = simpleColors[randomAlpha];
+  }
+  // console.log(colrArr);
+  color.textContent = colrArr;
+  // console.log(val);
+  document.body.style.backgroundColor = colrArr;
+}
